326be2345d51fe52fc1e74cc739cedd40d4e7db9
Nazar Omelchuk <zen2000ua@gmail.com>
2019-07-13 18:53:58 +0300

    updated

--- FL11_HW8/FL11_HW8/homework/js/reverseNumber.js
+++ FL11_HW8/FL11_HW8/homework/js/reverseNumber.js
@@ -1,5 +1,11 @@
-function reverseNumber(num) {
-    let revNum = +Math.abs(num).toString().split('').reverse().join('');
-    return num < 0 ? -revNum : revNum;
-}
-reverseNumber();
+function reverseNumber(n){
+    let reversed=0;
+    while(n!==0){
+      reversed*=10;
+      reversed+=n %10;
+      n-=n %10;
+      n/=10;
+    }
+    return reversed;
+  }
+  reverseNumber();
